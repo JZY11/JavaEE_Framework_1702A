136a26a97fc397da718a1c8e220a977b543b07e5
Tony.Jaa <1291813139@qq.com>
2017-07-07 11:13:35 +0800

    updata

--- src/main/java/demo/service/UserService.java
+++ src/main/java/demo/service/UserService.java
@@ -17,19 +17,19 @@ public class UserService {
     private  InterfaceTest interfaceTest;
 
     private int createUser(User user) {
-        return interfaceTest.createUser();
+        return interfaceTest.createUser(user);
     }
     private int updateUser(User user){
-        return interfaceTest.updateUser();
+        return interfaceTest.updateUser(user);
     }
-    private int deleteUser() {
-        return interfaceTest.deleteUser();
+    private int deleteUser(int id) {
+        return interfaceTest.deleteUser(id);
     }
     private List<User> queryAll() {
         return interfaceTest.queryAll();
     }
-    private User queryById(){
-        return interfaceTest.queryById();
+    private User queryById(int id){
+        return interfaceTest.queryById(id);
     }
 
 
