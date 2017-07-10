c30c0be20bab5f0aa6398f167d4e3c1dc09e7870
Tony.Jaa <1291813139@qq.com>
2017-07-10 17:28:14 +0800

    updata

---
+++ src/main/java/demo/controller/UserController.java
@@ -0,0 +1,21 @@
+package demo.controller;
+
+import demo.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/7/10.
+ * JavaEE_Framework_1702A.
+ */
+@Controller
+@RequestMapping
+public class UserController {
+    @RequestMapping("create")
+    private String create(User user){
+        //todo save user ...?
+        System.out.println("create....");
+        return "redirect:/index.jsp";
+    }
+}
