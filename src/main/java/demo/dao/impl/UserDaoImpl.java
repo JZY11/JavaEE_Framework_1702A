30266df69e57e08d6d7fece193d40ffbc8d0cdd0
Tony.Jaa <1291813139@qq.com>
2017-07-11 14:33:48 +0800

    updata

--- src/main/java/demo/dao/impl/UserDaoImpl.java
+++ src/main/java/demo/dao/impl/UserDaoImpl.java
@@ -1,9 +1,27 @@
 package demo.dao.impl;
 
+import demo.dao.UserDao;
+import demo.model.User;
+import demo.util.MyBatisSession;
+import org.apache.ibatis.session.SqlSession;
+
 /**
  * Created by zhenya.1291813139.com
  * on 2017/7/11.
  * JavaEE_Framework_1702A.
  */
-public class UserDaoImpl {
+public class UserDaoImpl implements UserDao{
+    @Override
+    public void creat(User user) {
+        try(SqlSession sqlSession = MyBatisSession.getSqlSession(true)){
+            sqlSession.insert("user.create",user);
+        }
+    }
+
+    @Override
+    public User signIn(User user) {
+        try(SqlSession sqlSession = MyBatisSession.getSqlSession(false)){
+            return sqlSession.selectOne("user.signIn",user);
+        }
+    }
 }
