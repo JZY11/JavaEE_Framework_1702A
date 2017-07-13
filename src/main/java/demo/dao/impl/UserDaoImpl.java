d8841050f907bea355fdbf463ea26239288f1194
Tony.Jaa <1291813139@qq.com>
2017-07-13 10:03:32 +0800

    updata

--- src/main/java/demo/dao/impl/UserDaoImpl.java
+++ src/main/java/demo/dao/impl/UserDaoImpl.java
@@ -4,12 +4,14 @@ import demo.dao.UserDao;
 import demo.model.User;
 import demo.util.MyBatisSession;
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by zhenya.1291813139.com
  * on 2017/7/12.
  * JavaEE_Framework_1702A.
  */
+@Repository
 public class UserDaoImpl implements UserDao{
 
     @Override
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -8,7 +8,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>主页</title>
+    <title>index page</title>
 </head>
 <body>
 <h1>主页</h1>
