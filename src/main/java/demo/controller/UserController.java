a95816376a6c3e166e3617d940ffc107d2f61a42
Tony.Jaa <1291813139@qq.com>
2017-07-14 09:41:38 +0800

    updata

--- src/main/java/demo/controller/UserController.java
+++ src/main/java/demo/controller/UserController.java
@@ -38,6 +38,12 @@ public class UserController extends BaseController{
         return "/default.jsp";
     }
 
+    @RequestMapping("signOut")
+    private String signOut() {
+        session.invalidate();
+        return "redirect:/default.jsp";
+    }
+
 
 
 
