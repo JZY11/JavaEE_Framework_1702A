f5588e3bb279f5952c57fa5a025732b88f9feefe
Tony.Jaa <1291813139@qq.com>
2017-07-18 10:51:44 +0800

    updata

---
+++ src/main/java/demo/service/BookGenericService.java
@@ -0,0 +1,11 @@
+package demo.service;
+
+import demo.model.Book;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/7/18.
+ * JavaEE_Framework_1702A.
+ */
+public interface BookGenericService extends GenericService<Book> {
+}
